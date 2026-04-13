03ba8d188c22ca71504a2e4afd1a7ce47bdcfc54
whitedragon115 <stdn1255010@goo.tyai.tyc.edu.tw>
2026-04-13 21:34:24 +0800

    create trigger builder script and example

---
+++ trigger/buttons/push.js
@@ -0,0 +1,11 @@
+module.exports = {
+    customId: 'push',
+
+    /**
+     * @param {import('discord.js').ButtonInteraction} interaction
+     * @param {import('discord.js').Client} client
+     */
+    async execute(interaction, client) {
+        await interaction.reply({ content: 'Button pushed!', ephemeral: true });
+    }
+}
---
+++ trigger/modal/submit.js
@@ -0,0 +1,11 @@
+module.exports = {
+    customId: 'submit',
+
+    /**
+     * @param {import('discord.js').ModalSubmitInteraction} interaction
+     * @param {import('discord.js').Client} client
+     */
+    async execute(interaction, client) {
+        await interaction.reply({ content: 'Modal submitted!', flags: 'Ephemeral' });
+    }
+}
